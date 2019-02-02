611318e4e0802d2114d675b2f47c8e3cf7a9f76e
Kurus Sethna <kuruss@gmail.com>
2019-02-03 03:41:21 +0530

    Basic Environment
    Track position of hand

---
+++ BE_Project/Assets/Project/Prefabs/SimpleCarController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    public WheelCollider frontRightWheel, frontLeftWheel, rearFrontWheel, rearLeftWheel;
+    //public Transform frontRightWheelTransform, frontLeftWheelTransform, rearFrontWheelTransform, rearLeftWheelTransform;
+    public float maxSteeringAngle = 30.0f;
+    public float motorForce = 50.0f;
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteeringAngle * horizontalInput;
+
+        frontLeftWheel.steerAngle = steeringAngle;
+        frontRightWheel.steerAngle = steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontRightWheel.motorTorque = verticalInput * motorForce;
+        frontLeftWheel.motorTorque = verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
+}
