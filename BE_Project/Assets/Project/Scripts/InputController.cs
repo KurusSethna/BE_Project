6feb40266b92ae95367a4a3cea9f2061659df0d4
Kurus Sethna <kuruss@gmail.com>
2019-03-22 00:17:11 +0530

    Completed driving

--- BE_Project/Assets/Project/Scripts/InputController.cs
+++ BE_Project/Assets/Project/Scripts/InputController.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class InputController : MonoBehaviour
 {
 
+    // Serialized Game Objects
+
     [SerializeField]
     private GameObject handMarker;
     [SerializeField]
@@ -13,11 +15,17 @@ public class InputController : MonoBehaviour
     [SerializeField]
     private GameObject notification;
 
+    // Private variables
+
     private float zAxisValue, xAxisValue;
 
-    public int horizontalAxis, verticalAxis;
+    // Public variables
+
+    public float horizontalAxis, verticalAxis;
 
     public bool isStarted;
+
+    // Creating static instance of class
 
     public static InputController instance;
 
@@ -37,6 +45,7 @@ public class InputController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize Values
         xAxisValue = 0;
         zAxisValue = 0;
         isStarted = false;
@@ -45,44 +54,28 @@ public class InputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get position of hand marker local to camera
         zAxisValue = handMarker.transform.localPosition.z - 3.5f;
         xAxisValue = handMarker.transform.localPosition.x;
 
+        // Display position of hand marker
         zAxisText.text = "Z: " + zAxisValue.ToString("0.00");
         xAxisText.text = "X: " + xAxisValue.ToString("0.00");
 
-        if (zAxisValue > 0.5)
-        {
-            verticalAxis = 1;
-        }
-        else if (zAxisValue < -0.5)
-        {
-            verticalAxis = -1;
-        }
-        else
-        {
-            verticalAxis = 0;
-        }
-
-        if (xAxisValue > 0.5)
-        {
-            horizontalAxis = 1;
-        }
-        else if (xAxisValue < -0.5)
-        {
-            horizontalAxis = -1;
-        }
-        else
-        {
-            horizontalAxis = 0;
-        }
+        // Obtain input values for horizontal and vertical axis after normalising hand marker position values
+        verticalAxis = Mathf.Clamp(zAxisValue, -3.5f, 3.5f) / 3.5f;
+        horizontalAxis = Mathf.Clamp(xAxisValue, -3.5f, 3.5f) / 3.5f;
 
     }
 
+    // Notifies user when hand marker enters and exits null zone
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "RootPosition")
+        if (other.tag == "Null Point")
         {
+            // Begins simulation only when hand marker enters null zone for the first time
+
             Debug.Log("In Null Zone");
             notification.SetActive(true);
             isStarted = true;
